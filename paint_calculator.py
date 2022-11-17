e0e8d9a1cb75057a85362f5d5966ecce4d03f629
djr111 <58305266+djr111@users.noreply.github.com>
2022-11-17 17:33:30 +0000

    First commit

--- paint_calculator.py
+++ paint_calculator.py
@@ -1,33 +1,40 @@
-def wall(a, b):
+print ("Welcome to Paint-calculator")
 
-    area = a * b
-    return area
+def wall():
+    wall_width = input("Please enter wall width you want to paint: ")
+    wall_height = input("Please enter wall height you want to paint: ")
+    wall_area = float(wall_width) * float(wall_height)
+    print("Total surface area of wall in square meters : " + str(wall_area))
+    return wall_area
+
+
+def socket():
+    socket_width = input ("Please enter socket's width you want to paint: ")
+    socket_height = input ("Please enter socket's height you want to paint: ")
+    socket_area = float(socket_width) * float(socket_height)
+    print("Total surface area of sockets in square meters : " + str(socket_area))
+    return socket_area
 
 
 def door():
-    door_width = input ("Please enter door width :")
-    door_height = input ("Please enter door height :")
+    door_width = input ("Please enter door width you want to paint: ")
+    door_height = input ("Please enter door height you want to paint: ")
     door_area = float(door_width) * float(door_height)
-    print("Door size in square meters : " + str(door_area))
+    print("Total surface area of door in square meters : " + str(door_area))
     return door_area
 
 
-
-
 def window():
-    window_width = input ("Please enter window width")
-    window_height = input("Please enter window height")
+    window_width = input ("Please enter window width you want to paint: ")
+    window_height = input("Please enter window height you want to paint: ")
     window_area = float(window_height) * float(window_width)
-    print("Window size in square meters:" + str(window_area))
+    print("Total surface area of window in square meters:" + str(window_area))
     return window_area
 
 
-
 def paint():
-    wall_height = float(input("Enter height of the wall in meters: "))
-    wall_lenght = float(input("Enter lenght of the wall in meters: "))
 
-    paint_needed = (wall(wall_lenght, wall_height) - door() - window()) / 6
+    paint_needed = (wall() - door() - window() - socket()) / 6
     return paint_needed
 
 
