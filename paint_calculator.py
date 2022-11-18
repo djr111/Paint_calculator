4ec0ca55022fa32e95911c941fa9f2d293c10ae5
djr111 <58305266+djr111@users.noreply.github.com>
2022-11-18 15:12:22 +0000

    Added bucket calculation function, bucket dictionary, user input checks with key word GO to proceed, future work commented

--- paint_calculator.py
+++ paint_calculator.py
@@ -1,8 +1,20 @@
+import math
+
 print("Welcome to Paint-calculator")
+
+
+while True:
+    proceed = input("Please type GO to proceed further with program : ")
+    if proceed == "GO":
+        break
+    else:
+        print("Wrong input Robot  ")
 
 
 def wall():
     wall_width = input("Please enter wall width you want to paint: ")
+#    if not isinstance(wall_width, float):
+#        raise TypeError('wall width should be number')
     wall_height = input("Please enter wall height you want to paint: ")
     wall_number = input("How many walls you have to paint?: ")
     wall_area = float(wall_width) * float(wall_height) * int(wall_number)
@@ -11,8 +23,8 @@ def wall():
 
 
 def socket():
-    socket_width = input ("Please enter socket's width : ")
-    socket_height = input ("Please enter socket's height : ")
+    socket_width = input("Please enter socket's width : ")
+    socket_height = input("Please enter socket's height : ")
     socket_number = input("How many sockets you have ?: ")
     socket_area = float(socket_width) * float(socket_height) * int(socket_number)
     print("Total surface area of sockets in square meters : " + str(socket_area))
@@ -20,8 +32,8 @@ def socket():
 
 
 def door():
-    door_width = input ("Please enter door width : ")
-    door_height = input ("Please enter door height : ")
+    door_width = input("Please enter door width : ")
+    door_height = input("Please enter door height : ")
     door_number = input("How many doors you have ?: ")
     door_area = float(door_width) * float(door_height) * int(door_number)
     print("Total surface area of door in square meters : " + str(door_area))
@@ -29,7 +41,7 @@ def door():
 
 
 def window():
-    window_width = input ("Please enter window width : ")
+    window_width = input("Please enter window width : ")
     window_height = input("Please enter window height : ")
     window_number = input("How many windows you have ?: ")
     window_area = float(window_height) * float(window_width) * int(window_number)
@@ -42,11 +54,53 @@ def coats_of_paint():
     return coat_amount
 
 
-def paint():
+paint_cans = {
+    2.5: 30,
+    5: 60,
+    10: 120
+}
+# Dulux Silk Paint - Amazon
 
-    paint_needed = (wall() - door() - window() - socket()) / 6 * (coats_of_paint())
-    return paint_needed
+
+def painting():
+    #   Modern Emulsion 60 m2 per 5 litres / 30m2 per 2.5 litres
+    paint_needed = (wall() - door() - window() - socket()) / 12 * (coats_of_paint())
+    return round(paint_needed)
 
 
-print("You will need : " + str(paint()) + " amount of cans to paint")
+def paint_cans_needed(area):
+    big = 0
+    medium = 0
+    small = 0
+    one = 0
 
+    if area >= 10:
+        big += math.floor(area/10)
+        area -= math.floor(area/10) * 10
+    if area >= 5:
+        medium += math.floor(area/5)
+        area -= math.floor(area/5) * 5
+    if area >= 2.5:
+        small += math.floor(area/2.5)
+        area -= math.floor(area/2.5) * 2.5
+
+ #   if area > 0:
+ #       one += math.ceil(area/1)
+ #       area -= math.ceil(area/1) * 1
+
+    return ["10l --: " + str(big), "5l --: " + str(medium), "2.5l --: " + str(small)]
+
+
+area1 = painting()
+print("You will need : " + str(area1) + " amount of litres of paint")
+print("Paint cans needed : " + str(paint_cans_needed(area1)) + " for the job done")
+
+
+#  valid = False
+#   while not valid:
+#       try:
+#            type(wall_width) == float
+#           valid = True
+#      except ValueError:
+#           print("Entered value is not number")
+# return wall_width
